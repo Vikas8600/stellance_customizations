ee8e0bef586783d6576c56c8c74f52f7161a3c1d
TruptiNi <trupti.n@logicaldna.com>
2025-01-03 16:00:19 +0530

    |#stellence_customization|Add doctypes|

---
+++ stellance_customizations/stellance_customizations/doctype/packs/packs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, chinmay@hybrowlabs.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Packs", {
+// 	refresh(frm) {
+
+// 	},
+// });
