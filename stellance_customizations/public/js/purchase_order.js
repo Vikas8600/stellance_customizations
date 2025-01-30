71033547d583bd16aae59e32b9215b161090c865
Vikas <vikas.moin777@gmail.com>
2025-01-30 22:29:58 +0530

    Refactor Purchase Order Item to update custom bundle size options and enhance status options retrieval

--- stellance_customizations/public/js/purchase_order.js
+++ stellance_customizations/public/js/purchase_order.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on('Purchase Order', {
-	refresh: {
+	refresh: function(frm){
+        update_status_options(frm)
     }
 });
 
@@ -20,7 +21,8 @@ frappe.ui.form.on('Purchase Order Item', {
                         var bundle_sizes = item.custom_item_pack_size.map(function(pack) {
                             return pack.bundle_size;
                         });
-                        frm.fields_dict.items.grid.update_docfield_property("custom_bundle_sizeuom","options",bundle_sizes);
+                        frappe.utils.filter_dict(cur_frm.fields_dict["items"].grid.grid_rows_by_docname[child.name].docfields, { "fieldname": "custom_bundle_sizeuom" })[0].options = bundle_sizes;
+                        // frm.fields_dict.items.grid.update_docfield_property("custom_bundle_sizeuom","options",bundle_sizes);
                     }
                 },
                 
@@ -28,3 +30,30 @@ frappe.ui.form.on('Purchase Order Item', {
         }
     }
 });
+
+function update_status_options(frm, cdt, cdn){
+    let child_items = frm.doc.items || [];
+        child_items.forEach(row => {
+            if(row.item_code){
+                frappe.call({
+                    method: 'frappe.client.get',
+                    args: {
+                        doctype: 'Item',
+                        name: row.item_code
+                    },
+                    callback: function(response) {
+                        var item = response.message;
+                        if (item.custom_item_pack_size) {
+                            var bundle_sizes = item.custom_item_pack_size.map(function(pack) {
+                                return pack.bundle_size;
+                            }); 
+                            frappe.utils.filter_dict(cur_frm.fields_dict["items"].grid.grid_rows_by_docname[row.name].docfields, { "fieldname": "custom_bundle_sizeuom" })[0].options = bundle_sizes;
+                            // cur_frm.refresh();
+                            // frm.fields_dict.items.grid.update_docfield_property("custom_bundle_sizeuom","options",bundle_sizes);
+                        }
+                    },
+                    
+                });
+        }
+        });
+}
